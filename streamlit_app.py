294d6b11fc04a8da552409b5aacdc1a2e7b9a7f0
k.nonaka <opoblblfozb@gmail.com>
2022-07-23 20:55:02 +0900

    hogehoge

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,22 @@
 import streamlit as st
 
+from matplotlib import pyplot as plt
+import japanize_matplotlib
+
 st.title("ゆきちゃん大好き！ピヨピヨ")
 st.markdown(
     """
     hogehoge
     """
 )
+
+def exp(x):
+    return [10 ** t for t in x]
+
+x = [i /30 for i in range(50)]
+fig, ax = plt.subplots()
+ax.plot(x, exp(x))
+ax.set_title("ゆきちゃんへの溺愛度")
+ax.set_xlabel("時間")
+ax.set_ylabel("溺愛度")
+st.pyplot(fig)
